ae4b6975c9625a467b5694c7ba2fe6b72bad75cf
0xf1f0 <oobey1@aol.com>
2017-07-24 20:51:23 -0400

    Add DifferenceOfGaussian

--- app/src/main/java/com/example/sudokuer/sudokuer/Image.java
+++ app/src/main/java/com/example/sudokuer/sudokuer/Image.java
@@ -10,6 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
@@ -22,12 +32,60 @@ public class Image extends AppCompatActivity
     private final String TAG = getClass().getSimpleName();
 
     private Button mDoneBtn;
-    private Bitmap bitmap;
+    private Bitmap mBitmap;
 
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this)
+    {
+        @Override
+        public void onManagerConnected(int status)
+        {
+            Log.i(TAG, "Calling -> onManagerConnected()");
+            switch (status)
+            {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    try
+                    {
+                        DifferenceOfGaussian();
+                    } catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        Log.i(TAG, "Calling -> onResume()");
+        super.onResume();
+        if (!OpenCVLoader.initDebug())
+        {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for " +
+                    "initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else
+        {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        Log.i(TAG, "Calling -> onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
@@ -52,7 +110,6 @@ public class Image extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                bitmap.recycle();
                 finish();
             }
         });
@@ -61,11 +118,11 @@ public class Image extends AppCompatActivity
     }
 
     /**
-     * Display a bitmap image capture or loaded from gallery on screen
+     * Display a mBitmap image capture or loaded from gallery on screen
      */
     private void handleImageIntent(Uri imageUri)
     {
-        mImageView = (ImageView) findViewById(R.id.display_image);
+        Log.i(TAG, "Calling -> handleImageIntent()");
         InputStream inputStream;
 
         if (imageUri != null)
@@ -75,10 +132,10 @@ public class Image extends AppCompatActivity
                 inputStream = getContentResolver().openInputStream(imageUri);
                 if (inputStream != null)
                 {
-                    bitmap = BitmapFactory.decodeStream(inputStream);
-                    if (bitmap != null)
+                    mBitmap = BitmapFactory.decodeStream(inputStream);
+                    if (mBitmap != null)
                     {
-                        mImageView.setImageBitmap(bitmap);
+                        loadImageToImageView();
                     }
                 }
             } catch (FileNotFoundException e)
@@ -86,5 +143,45 @@ public class Image extends AppCompatActivity
                 Log.e(TAG, e.getMessage(), e);
             }
         }
+    }
+
+    public void DifferenceOfGaussian()
+    {
+        Log.i(TAG, "Calling -> DifferenceOfGaussian()");
+        Mat grayMat = new Mat();
+        Mat blur1 = new Mat();
+        Mat blur2 = new Mat();
+        Mat imageMat = new Mat();
+        //Covert mBitmap to Mat
+
+
+        //Converting Bitmap back to Mat
+        Utils.bitmapToMat(mBitmap, imageMat);
+
+        //Converting the image to grayscale
+        Imgproc.cvtColor(imageMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        //Blurring the images using two different blurring radius
+        Imgproc.GaussianBlur(grayMat, blur1, new Size(15, 15), 5);
+        Imgproc.GaussianBlur(grayMat, blur2, new Size(21, 21), 5);
+
+        //Subtracting the two blurred images
+        Mat DoG = new Mat();
+        Core.absdiff(blur1, blur2, DoG);
+
+        //Inverse Binary Thresholding
+        Core.multiply(DoG, new Scalar(100), DoG);
+        Imgproc.threshold(DoG, DoG, 50, 255, Imgproc.THRESH_BINARY_INV);
+
+        //Convert mat to bitmap
+        Utils.matToBitmap(DoG, mBitmap);
+    }
+
+    private void loadImageToImageView()
+    {
+        Log.i(TAG, "Calling -> loadImageToImageView()");
+
+        mImageView = (ImageView) findViewById(R.id.display_image);
+        mImageView.setImageBitmap(mBitmap);
     }
 }
